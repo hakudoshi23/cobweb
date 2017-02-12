18a4394caf9fedc7051998dcc92b625a9bd56263
avalls <aleixvalls14@gmail.com>
2017-02-12 18:36:16 +0100

    logger now supports multiple parameters

--- src/logger.js
+++ src/logger.js
@@ -6,41 +6,41 @@
     var Logger = function (instance) {
         this.handlers = [];
 
-        this.addHandler(this.level.ALL, function (level, message) {
+        this.addHandler(this.level.ALL, function (level, args) {
             if (Logger.prototype.level.DEBUG === level) {
-                console.debug(message);
+                console.debug.apply(this, args);
             } else if (Logger.prototype.level.INFO === level) {
-                console.info(message);
+                console.info.apply(this, args);
             } else if (Logger.prototype.level.WARNING === level) {
-                console.warn(message);
+                console.warn.apply(this, args);
             } else if (Logger.prototype.level.ERROR === level) {
-                console.error(message);
+                console.error.apply(this, args);
             }
         });
     };
 
-    Logger.prototype.debug = function (message) {
-        this.log(Logger.prototype.level.DEBUG, message);
+    Logger.prototype.debug = function () {
+        this.log(Logger.prototype.level.DEBUG, arguments);
     };
 
-    Logger.prototype.info = function (message) {
-        this.log(Logger.prototype.level.INFO, message);
+    Logger.prototype.info = function () {
+        this.log(Logger.prototype.level.INFO, arguments);
     };
 
-    Logger.prototype.warning = function (message) {
-        this.log(Logger.prototype.level.WARNING, message);
+    Logger.prototype.warning = function () {
+        this.log(Logger.prototype.level.WARNING, arguments);
     };
 
-    Logger.prototype.error = function (message) {
-        this.log(Logger.prototype.level.ERROR, message);
+    Logger.prototype.error = function () {
+        this.log(Logger.prototype.level.ERROR, arguments);
     };
 
-    Logger.prototype.log = function (level, message) {
+    Logger.prototype.log = function (level, args) {
         var tmp = level;
         var levelHandlers = this.handlers[tmp] || [];
         while (tmp >= 0) {
             levelHandlers.forEach(function (handler) {
-                handler(level, message);
+                handler(level, args);
             });
             levelHandlers = this.handlers[--tmp] || [];
         }
