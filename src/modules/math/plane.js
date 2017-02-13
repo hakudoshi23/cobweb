9292a0d734987446aa5e8f97da7639dff0b4e854
avalls <aleixvalls14@gmail.com>
2017-02-13 22:36:57 +0100

    simple Ray & Plane prototypes

---
+++ src/modules/math/plane.js
@@ -0,0 +1,13 @@
+((function () {
+    'use strict';
+
+    window.Math = window.Math || {};
+
+    var Plane = function (point, normal) {
+        this.normal = normal || [0, 1, 0];
+        this.point = point || vec3.create();
+        vec3.normalize(this.normal, this.normal);
+    };
+
+    window.Math.Plane = Plane;
+})());
---
+++ src/modules/math/ray.js
@@ -0,0 +1,19 @@
+((function () {
+    'use strict';
+
+    window.Math = window.Math || {};
+
+    var Ray = function (start, direction) {
+        this.direction = direction || [0, 0, 1];
+        this.start = start || vec3.create();
+        vec3.normalize(this.direction, this.direction);
+    };
+
+    Ray.fromPoints = function (a, b) {
+        var direction = vec3.create();
+        vec3.sub(direction, b, a);
+        return new Ray(a, direction);
+    };
+
+    window.Math.Ray = Ray;
+})());
